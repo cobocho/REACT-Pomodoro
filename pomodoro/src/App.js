52ffa1952e77056cadc24786fc1a697bcafe2a43
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-01-05 17:00:18 +0900

    feat: 제목 및 타이머 샘플 구현

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Header from "./Components/Header/Header";
+import Timer from "./Components/Timer/Timer";
+
+import { Fragment } from "react";
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <Header />
+      <Timer />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ pomodoro/src/Components/Timer/Timer.js
@@ -0,0 +1,11 @@
+import classes from "./Timer.module.css";
+
+const Timer = (props) => {
+  return (
+    <section className={classes.timer}>
+      <strong>00 : 00</strong>
+    </section>
+  );
+};
+
+export default Timer;
